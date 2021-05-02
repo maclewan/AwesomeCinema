2b8dafab51c0db139b17bd26ce5231e7235846c1
mihal09 <janikmichu@gmail.com>
2021-05-02 18:44:39 +0200

    feat: display flatlist, API

---
+++ AwesomeCinema/config.js
@@ -0,0 +1,1 @@
+export const API_KEY = 'cb944a6fe704dd0999df599cfbcce7d2';
--- AwesomeCinema/src/screens/MovieListScreen.js
+++ AwesomeCinema/src/screens/MovieListScreen.js
@@ -1,10 +1,106 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  FlatList,
+  Dimensions,
+  Image,
+  StatusBar,
+} from 'react-native';
+import {getMovies} from '../api/tmdbAPI';
+import Genres from '../components/Genres';
+import Rating from '../components/Rating';
 
-const MovieListScreen = () => (
-  <View>
-    <Text>MovieListScreen</Text>
+const {width, height} = Dimensions.get('window');
+const SPACING = 10;
+const ITEM_SIZE = width * 0.72;
+
+const Loading = () => (
+  <View style={styles.loadingContainer}>
+    <Text style={styles.paragraph}>Loading...</Text>
   </View>
 );
+
+const MovieListScreen = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const movies = await getMovies();
+      setMovies(movies);
+    };
+
+    if (movies.length === 0) {
+      fetchData(movies);
+    }
+  }, [movies]);
+
+  if (movies.length === 0) {
+    return <Loading />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <StatusBar hidden />
+      <FlatList
+        showsHorizontalScrollIndicator={false}
+        data={movies}
+        keyExtractor={item => item.key}
+        horizontal
+        contentContainerStyle={{alignItems: 'center'}}
+        renderItem={({item}) => {
+          return (
+            <View style={{width: ITEM_SIZE}}>
+              <View
+                style={{
+                  marginHorizontal: SPACING,
+                  padding: SPACING * 2,
+                  alignItems: 'center',
+                  backgroundColor: 'white',
+                  borderRadius: 34,
+                }}>
+                <Image source={{uri: item.poster}} style={styles.posterImage} />
+                <Text style={{fontSize: 24}} numberOfLines={1}>
+                  {item.title}
+                </Text>
+                <Rating rating={item.rating} />
+                <Genres genres={item.genres} />
+                <Text style={{fontSize: 12}} numberOfLines={3}>
+                  {item.description}
+                </Text>
+              </View>
+            </View>
+          );
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container: {
+    flex: 1,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  posterImage: {
+    width: '100%',
+    height: ITEM_SIZE * 1.2,
+    resizeMode: 'cover',
+    borderRadius: 24,
+    margin: 0,
+    marginBottom: 10,
+  },
+});
 
 export default MovieListScreen;
